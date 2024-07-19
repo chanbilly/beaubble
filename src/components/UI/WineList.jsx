3ad33f10a6b478424f386bb26f00118b4a21e479
Billy Chan <chanbilly@icloud.com>
2024-07-19 10:51:37 +0900

    Add staggering animation for wine list

--- src/components/UI/WineList.jsx
+++ src/components/UI/WineList.jsx
@@ -7,20 +7,51 @@ export default function WineList() {
   const wines = useAppStore((state) => state.wines)
   const wineList = useRef(null)
   const isInView = useInView(wineList)
+  const [isVisible, setIsVisible] = useState(false) 
 
   useEffect(() => {
-    console.log("Element is in view: ", isInView)
+    if(isInView) {
+      setIsVisible(true)
+    } else {
+      setIsVisible(false)
+    }
   }, [isInView])
 
   return (
-    <div className="wine-list_ctn mt-6" >
-      <ul ref={wineList}>
+    <div className="wine-list mt-6" >
+      <motion.ul className="wine-list_ctn" ref={wineList} variants={list} initial="hidden" animate={isVisible? "visible" : "hidden"}>
         {wines.map((wine, index) => (
-          <motion.li key={index}>
+          <motion.li key={index} variants={items}> 
             {wine}
           </motion.li>
         ))}
-      </ul>
+      </motion.ul>
     </div>
   )
 }
+
+const list = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+      ease: [0.46,0.03,0.52,0.96],
+      duration: 0.6,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: 20,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+}
+
+const items = {
+  visible: { opacity: 1, y: 0 },
+  hidden: { opacity: 0, y: 20 },
+}
+
